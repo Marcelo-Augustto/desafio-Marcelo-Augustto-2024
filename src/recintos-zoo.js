400a255bfaa2240f2080567567037d4e0bac96c0
Marcelo-Augustto <3augustto3@gmail.com>
2024-09-10 00:29:06 -0300

    last rules implemented

--- src/recintos-zoo.js
+++ src/recintos-zoo.js
@@ -1,4 +1,5 @@
 const animaisValidos = ['LEAO','LEOPARDO','CROCODILO','MACACO','GAZELA','HIPOPOTAMO'];
+const animaisCarnivoros = ['LEAO', 'LEOPARDO', 'CROCODILO'];
 
 class RecintosZoo {
 
@@ -20,7 +21,7 @@ class RecintosZoo {
                 id: 3,
                 bioma: ['SAVANA', 'RIO'],
                 tamanho: 7,
-                animais: ['GAZELA']
+                animais: ['GAZELA', 'GAZELA']
             },
             {
                 id: 4,
@@ -32,7 +33,7 @@ class RecintosZoo {
                 id: 5,
                 bioma: ['SAVANA'],
                 tamanho: 9,
-                animais: ['LEAO']
+                animais: ['LEAO','LEAO','LEAO']
             }
         ];
     }
@@ -43,7 +44,7 @@ class RecintosZoo {
             return {
                 erro: 'Animal inválido',
                 recintosViaveis: null
-            };
+            }
         } else if (quantidade == 0) {
             return {
                 erro: 'Quantidade inválida',
@@ -59,21 +60,62 @@ class RecintosZoo {
     }
 
     checarEspaco(recintos, animal, quantidade) {
-        let check = 0;
-        recintos.forEach(e => {
-            for (let i in e.bioma) {
-                if (animal.bioma.includes(e.bioma[i])) {
-                    let tamanhoEstimado = animal.tamanho * quantidade + e.animais.length;
-                    if (tamanhoEstimado > e.tamanho) {
-                        check = {
-                            erro: 'Não há recinto viável',
-                            recintosViaveis: null
-                        }
-                        break;
+        let check = null;
+        let recintosViaveis = [];
+
+        recintos.forEach((e, i) => {
+            let tamanhoEstimado = 0;
+            for (let j in e.bioma) {
+                if (animal.bioma.includes(e.bioma[j])) {
+                    tamanhoEstimado = animal.tamanho * quantidade + e.animais.length;
+                    if (!e.animais.includes(animal.especie) && e.animais.length > 0) {
+                        tamanhoEstimado ++;
                     }
+                    break;
                 }
             }
+            if (tamanhoEstimado <= e.tamanho && tamanhoEstimado > 0) {
+                if (animal.carnivoro) {
+                    if (e.animais.includes(animal.especie) || e.animais.length == 0) {
+                        recintosViaveis.push(`Recinto ${e.id} (espaço livre: ${e.tamanho - tamanhoEstimado} total: ${e.tamanho})`); 
+                    } 
+                } else {
+                    let recintoValido = true;
+                    for (let l in animaisCarnivoros) {
+                        if (e.animais.includes(animaisCarnivoros[l])) {
+                            recintoValido = false;
+                        }
+                    }
+                    if (animal.especie == 'MACACO' && e.animais.length == 0 && quantidade == 1) {
+                        recintoValido = false;
+                    }
+                    if (animal.especie == 'HIPOPOTAMO') {
+                        if (e.animais.length > 0 && !e.animais.includes(animal.especie)) {
+                            recintoValido = false
+                            
+                            if (e.bioma.includes('SAVANA') && e.bioma.includes('RIO')) {
+                                recintoValido = true;
+                            }
+                        }
+                    }
+                    if (recintoValido) {
+                        recintosViaveis.push(`Recinto ${e.id} (espaço livre: ${e.tamanho - tamanhoEstimado} total: ${e.tamanho})`); 
+                    }
+                }
+            } 
         });
+
+        if (recintosViaveis.length == 0) {
+            check = {
+                erro: 'Não há recinto viável',
+                recintosViaveis: null
+            }
+        } else {
+            check = {
+                recintosViaveis: recintosViaveis,
+                erro: null
+            }
+        }
         return check;
     }
 
@@ -85,32 +127,34 @@ class Animal {
             this.especie = especie;
             this.tamanho = 3;
             this.bioma = ['SAVANA'];
+            this.carnivoro = true;
         } else if (especie == 'LEOPARDO') {
             this.especie = especie;
             this.tamanho = 2;
             this.bioma = ['SAVANA'];
+            this.carnivoro = true;
         } else if (especie == 'CROCODILO') {
             this.especie = especie;
             this.tamanho = 3;
             this.bioma = ['RIO'];
+            this.carnivoro = true;
         } else if (especie == 'MACACO') {
             this.especie = especie;
             this.tamanho = 1;
             this.bioma = ['SAVANA','FLORESTA'];
+            this.carnivoro = false;
         } else if (especie == 'GAZELA') {
             this.especie = especie;
             this.tamanho = 2;
             this.bioma = ['SAVANA'];
+            this.carnivoro = false;
         } else if (especie == 'HIPOPOTAMO') {
             this.especie = especie;
             this.tamanho = 4;
             this.bioma = ['SAVANA','RIO'];
+            this.carnivoro = false;
         }
     }
 }
 
 export { RecintosZoo as RecintosZoo };
-
-// let resultado = new RecintosZoo().analisaRecintos('MACACO', 10)
-
-// console.log(resultado);
--- src/tempCodeRunnerFile.js
+++ src/tempCodeRunnerFile.js
@@ -1,15 +1,5 @@
-ch(e => {
-        //     
-        //     for (let i in e.bioma) {
-        //         if (animal.bioma.includes(e.bioma[i])) {
-        //             let tamanhoEstimado = animal.tamanho * quantidade + e.animais.length;
-        //             if (tamanhoEstimado > e.tamanho) {
-        //                 check = {
-        //                     erro: 'Não há recinto viável',
-        //                     recintosViaveis: null
-        //                 }
-        //                 break;
-        //             }
-        //         }
-        //     }
-        // });
+(animal.especie == 'HIPOPOTAMO') {
+                            //     if (!e.animais.includes(animal.especie) && e.animais.length > 0) {
+                            //         recintoValido = false;
+                            //     }
+                            // }
